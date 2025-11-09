47efda2afac0d8817a524c78b99ec58817924e05
fiviantulele <168805065+fiviantulele@users.noreply.github.com>
2025-11-09 09:27:43 +0300

    last

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig(({ mode }) => {
       host: "::",
       port: 8080,
     },
+    preview: {
+      allowedHosts: ["branch-wise-school-link.onrender.com"]
+    },
     plugins: [
       react(),
   // componentTagger removed
